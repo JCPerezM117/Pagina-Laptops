243560257ae42910df244db0039e82b00c9fd2c4
JCPerezM117 <rexjc1@hotmail.com>
2025-11-13 01:02:18 -0600

    Configuración para Render

--- 1_conexion.php
+++ 1_conexion.php
@@ -1,12 +1,28 @@
-<?php
-$host = "localhost";
-$user = "root";     // cambia si tienes otro usuario
-$pass = "";         // cambia si tu MySQL tiene contraseña
-$db   = "sistema_login";
+       <?php
+        /*$host = "localhost";
+        $user = "root";     // cambia si tienes otro usuario
+        $pass = "";         // cambia si tu MySQL tiene contraseña
+        $db   = "sistema_login";
 
-$conn = new mysqli($host, $user, $pass, $db);
+        $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("❌ Error de conexión: " . $conn->connect_error);
-}
-?>
+        if ($conn->connect_error) {
+            die("❌ Error de conexión: " . $conn->connect_error);
+        }*/
+              
+        // Si Render proporciona las variables de entorno, se usan.
+        // Si estás en local, se usan tus valores por defecto.
+        $host = getenv('DB_HOST') ?: 'localhost';
+        $user = getenv('DB_USER') ?: 'root';
+        $pass = getenv('DB_PASS') ?: '';
+        $db   = getenv('DB_NAME') ?: 'sistema_login';
+
+        $conn = new mysqli($host, $user, $pass, $db);
+
+        if ($conn->connect_error) {
+            die("❌ Error de conexión: " . $conn->connect_error);
+        }
+
+
+        ?>
+
